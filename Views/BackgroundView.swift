44d6c6cebc68346e356665db2a82ea72b2cadcbb
USER <>
2022-10-05 21:59:07 +0900

    before scrollview

--- Views/BackgroundView.swift
+++ Views/BackgroundView.swift
@@ -26,6 +26,7 @@ struct BackgroundView: View {
 
 struct TopView: View {
     @Binding var game: Game
+    @State private var leaderboardIsShowing = false
     
     var body: some View {
         HStack {
@@ -36,7 +37,11 @@ struct TopView: View {
             }
             
             Spacer()
+            Button(action: {leaderboardIsShowing = true
+                
+            } ) {
             RoundedImageViewFilled(systemName: "list.dash")
+            }.sheet(isPresented: $leaderboardIsShowing, onDismiss: {}, content: {LeaderBoardView(leaderboardIsShowing: $leaderboardIsShowing,game: $game)})
         }
         
         
--- Views/LeaderBoardView.swift
+++ Views/LeaderBoardView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct LeaderBoardView: View {
+    @Binding var leaderboardIsShowing: Bool
+    @Binding var game: Game
+    
     var body: some View {
-        RowView(index: 1, score: 10, date: Date())
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 10) {
+                HeaderView(leaderboardIsShowing: $leaderboardIsShowing)
+                LabelView()
+                    VStack(spacing: 10) {
+                        ForEach(game.leaderboardEntries.indices){ i in
+                            let leaderboardEntry = game.leaderboardEntries[i]
+                            RowView(index: i, score: leaderboardEntry.score, date: leaderboardEntry.date)
+                        
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -19,7 +36,7 @@ struct RowView: View {
     let date: Date
     var body: some View {
         HStack {
-    RoundedTextView(text: String(index))
+            RoundedTextView(text: String(index))
             Spacer()
             ScoreText(score: score)
                 .frame(width: Constant.Leaderboard.leaderboardScoreColWidth)
@@ -27,14 +44,14 @@ struct RowView: View {
             DateText(date: date)
                 .frame(width: Constant.Leaderboard.leaderboardDateColWidth)
             
-        
+            
         }
         
         .background(
             RoundedRectangle(cornerRadius:
                     .infinity)
-                .strokeBorder(Color("LeaderboardRowColor"),
-                              lineWidth: Constant.General.strokeWidth)
+            .strokeBorder(Color("LeaderboardRowColor"),
+                          lineWidth: Constant.General.strokeWidth)
         )
         .padding(.leading)
         .padding(.trailing)
@@ -42,17 +59,70 @@ struct RowView: View {
     }
 }
 
-struct LeaderBoardView_Previews: PreviewProvider {
-    static var previews: some View {
-        LeaderBoardView()
-        LeaderBoardView()
-            .previewLayout(.fixed(width: 568, height: 320))
-        LeaderBoardView()
-            .preferredColorScheme(.dark)
-        LeaderBoardView()
-            .preferredColorScheme(.dark)
-
-            .previewLayout(.fixed(width: 568, height: 320))
+struct HeaderView: View {
+    @Binding var leaderboardIsShowing: Bool
     
+    @Environment( \.verticalSizeClass) var verticalSizeClass
+    @Environment( \.horizontalSizeClass) var horizontalSizeClass
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                if verticalSizeClass == .regular && horizontalSizeClass == .compact {
+                    BigBoldText(text: "LeaderView")
+                        .padding(.leading)
+                    Spacer()
+                } else {
+                    BigBoldText(text: "LeaderView")
+                }
+            }
+            HStack {
+                Spacer()
+                Button(action: {leaderboardIsShowing = false} ) {
+                    RoundedImageViewFilled(systemName: "xmark")
+                        .padding(.trailing)
+                }
+                
+                
+            }
+        }
+    }
+}
+
+struct LabelView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+                .frame(width:
+                        Constant.General.roundedViewLenght)
+            Spacer()
+            LabelText(text: "Score")
+                .frame(width:Constant.Leaderboard.leaderboardScoreColWidth)
+            Spacer()
+            LabelText(text: "Date")
+                .frame(width: Constant.Leaderboard.leaderboardDateColWidth)
+        }
+        .padding(.leading)
+        .padding(.trailing)
+        .frame(maxWidth: Constant.Leaderboard.leaderboardMaxRowWidth)
+    }
+    
+    
+}
+
+struct LeaderBoardView_Previews: PreviewProvider {
+    static private var leaderboardIsShowing = Binding.constant(false)
+    static private var game = Binding.constant(Game(loadTestData: true))
+    static var previews: some View {
+        LeaderBoardView(leaderboardIsShowing: leaderboardIsShowing, game: game)
+        LeaderBoardView(leaderboardIsShowing: leaderboardIsShowing, game: game)
+            .previewLayout(.fixed(width: 568, height: 320))
+        LeaderBoardView(leaderboardIsShowing: leaderboardIsShowing, game: game)
+            .preferredColorScheme(.dark)
+        LeaderBoardView(leaderboardIsShowing: leaderboardIsShowing, game: game)
+            .preferredColorScheme(.dark)
+        
+            .previewLayout(.fixed(width: 568, height: 320))
+        
     }
 }
